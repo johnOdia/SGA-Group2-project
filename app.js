1e25a335a012f35230ff691c09a4ab8adb260010
johnOdia <efosajohnodia@gmail.com>
2020-09-16 15:10:47 -1100

    first commit

---
+++ app.js
@@ -0,0 +1,164 @@
+//GLOBAL VARIABLES
+let searchForChannel;
+let playListId;
+
+
+$(function () {
+    //key
+    const API_KEY = 'AIzaSyDbQowDf3wo5fsSXQlu2eRKskZ_er6RiIA'
+
+   
+    let videos = $('#videos')
+
+    //EVENT HANDLER
+    $('form').on('submit', handleFormSubmit)
+    $('#channelInfo').on('click', getVids)
+    $('#clearAll').on('click',clearAll)
+
+    // addChannel()
+
+
+    //get user input from form and pass it as an argument to the getChannel function which makes the a request to the API
+    function handleFormSubmit(e) {
+        e.preventDefault()
+        searchForChannel = $('input').val()
+        $('input').val('')
+        // $('#channelInfo').html('')
+
+        getChannel(searchForChannel)
+    }
+
+
+    //API REQUESTS
+
+    //get channel info from API
+    function getChannel(searchForChannel) {
+        $.get(`https://www.googleapis.com/youtube/v3/channels/?part=snippet%2CcontentDetails%2Cstatistics&forUsername=${searchForChannel}&maxResults=10&key=${API_KEY}`, function (response) {
+            if (response.pageInfo.totalResults === 0) alert('No Youtube channel by that name!')
+            console.log(response)
+            render(response)
+            addChannel(response.items[0])
+        })
+    }
+
+    function render(response) {
+        $.each(response.items, function (i, item) {
+            let output = `
+       <ul class='list-group'>
+         <li class='list-group-item li-top'><b>Title</b>: ${item.snippet.title}</li>
+         <li class='list-group-item'><b>Subscribers</b>: ${numberWithCommas(item.statistics.subscriberCount)}</li>
+         <li class='list-group-item'><b>Views</b>: ${numberWithCommas(item.statistics.viewCount)}</li>
+         <li class='list-group-item li-bottom'><b>Videos</b>: ${numberWithCommas(item.statistics.videoCount)}</li>
+         <button id='displayVideos'  class='btn btn-danger'>View videos</button>
+       </ul>
+     `
+            $('#channelInfo').append(output)
+           
+
+        })
+        
+    }
+
+
+    //get videos in  channel playlist from API
+    function getVids(e) {
+        videos.html('')
+        let title
+        let channels = getChannels()
+        if ($(e.target).text() === 'View videos'){
+             title = getTitle($(e.target).closest('ul').children('.li-top').text())
+        }
+        console.log(title)
+        channels.forEach(val => {
+            if(title === val.snippet.title){
+                console.log(val.snippet.title)
+                playListId = val.contentDetails.relatedPlaylists.uploads
+            }
+        })
+        
+        $.get(`https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=14&playlistId=${playListId}&key=${API_KEY}`,
+            function (data) {
+                $.each(data.items, function (i, item) {
+                    let videoId = item.snippet.resourceId.videoId
+
+                    let output = `<div style='display:inherit'> <iframe width="180px" height="auto" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>`
+
+                    $('#videos').append(output)
+                })
+            }
+        )
+    }
+
+
+
+    //HELPERS
+    function numberWithCommas(x) {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+
+    //set local storage
+    function getChannels() {
+        let channels
+        if (localStorage.getItem('channels') === null) {
+            channels = [];
+        } else {
+            channels = JSON.parse(localStorage.getItem('channels'));
+        }
+        // localStorage.setItem('tasks', JSON.stringify('tasks'));
+        return channels;
+    }
+
+    function addChannel(channel){
+        const channels = getChannels()
+        channels.push(channel);
+        localStorage.setItem('channels', JSON.stringify(channels));
+    }
+
+
+    let renderFroMStorage = (function(){
+        console.log(getChannels())
+        let channels = getChannels()
+        $.each(channels, function (i, item) {
+            playListId = item.contentDetails.relatedPlaylists.uploads
+
+            let output = `
+       <ul class='list-group'>
+         <li class='list-group-item li-top'><b>Title</b>: ${item.snippet.title}</li>
+         <li class='list-group-item'><b>Subscribers</b>: ${numberWithCommas(item.statistics.subscriberCount)}</li>
+         <li class='list-group-item'><b>Views</b>: ${numberWithCommas(item.statistics.viewCount)}</li>
+         <li class='list-group-item li-bottom'><b>Videos</b>: ${numberWithCommas(item.statistics.videoCount)}</li>
+         <button id='displayVideos'  class='btn btn-danger'>View videos</button>
+       </ul>
+     `
+            $('#channelInfo').append(output)
+            $('#displayVideos').on('click', getVids)
+
+        })
+    })() 
+
+    function getTitle(string){
+        let newString = ''
+        let splitted = [...string]
+        splitted.splice(0,7)
+        splitted.forEach(val => {
+            if(typeof(val) === 'string'){
+                newString += val
+            }
+        })
+        return newString
+    }
+
+    function clearAll(){
+        $('#channelInfo').html('')
+        videos.html('')
+        localStorage.clear()
+    }
+
+
+})
+
+
+
+
+
+
